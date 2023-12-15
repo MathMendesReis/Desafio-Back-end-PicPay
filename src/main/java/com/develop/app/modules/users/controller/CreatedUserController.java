0cfc03e51e7143adb1ca65407b9bfb2043504d4f
Matheus <matheusmendesreis@outlook.com>
2023-12-15 08:58:45 -0300

    feat:created usersController

---
+++ src/main/java/com/develop/app/modules/users/controller/CreatedUserController.java
@@ -0,0 +1,10 @@
+package com.develop.app.modules.users.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class CreatedUserController {
+  
+}
