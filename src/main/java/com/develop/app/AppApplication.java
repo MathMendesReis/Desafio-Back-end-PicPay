995c11ca17703d1cad911fbffbc15e06c5f305fd
Matheus <matheusmendesreis@outlook.com>
2023-12-15 08:59:48 -0300

    chore:config swagger

--- src/main/java/com/develop/app/AppApplication.java
+++ src/main/java/com/develop/app/AppApplication.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Nome do projeto", description = "API responsável pela gestão de vagas", version = "1"))
+@OpenAPIDefinition(info = @Info(title = "Pic Pay simplificado", description = "Fluxo de transferência entre dois usuários", version = "1"))
 public class AppApplication {
 
 	public static void main(String[] args) {
